2fa73f313749a4c2d8946657743d5a66eb26310d
David Ihl <daih1985@gmail.com>
2025-01-30 13:43:18 +0100

    Add check/delete

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -3,10 +3,15 @@ import { useTracker, useSubscribe } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
+import { Meteor } from 'meteor/meteor';
 
 export const App = () => {
   const isLoading = useSubscribe('tasks');
   const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const handleToggleChecked = ({ _id, isChecked }: any) =>
+    Meteor.callAsync('tasks.toggleChecked', { _id, isChecked });
+  const handleDelete = ({ _id }: any) =>
+    Meteor.callAsync('tasks.delete', { _id });
 
   if (isLoading()) {
     return <div>Loading...</div>;
@@ -19,7 +24,12 @@ export const App = () => {
 
       <ul>
         {tasks.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={handleToggleChecked}
+            onDeleteClick={handleDelete}
+          />
         ))}
       </ul>
     </div>
